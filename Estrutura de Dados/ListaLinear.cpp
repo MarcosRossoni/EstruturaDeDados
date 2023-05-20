223cec246d09f434a09f2e799a49ff883a1d7c0e
marcos <marcosrossoni9@gmail.com>
2023-05-20 15:29:54 -0300

    lista linear

---
+++ Estrutura de Dados/ListaLinear.cpp
@@ -0,0 +1,187 @@
+#include <conio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+struct elemento {
+    char conteudo;
+    elemento *proximo;
+};
+
+elemento *inicio = NULL;
+int numeroElementos = 0;
+
+elemento *criarNovo();
+void insereFim();
+void insereInicio();
+void inserePosicao();
+void limpaLista();
+void mensagem(const char* mensagem);
+void mostrarLista();
+void retiraIncio();
+void retiraFim();
+void retiraPosicao();
+
+main (void) {
+    setlocale(LC_ALL, "Portuguese");
+    char opcao;
+    do {
+        mostrarLista();
+        printf("\n\n\n              Menu\n");
+        printf("----------------------------\n");
+        printf("0 - Sair\n");
+        printf("1 - Limpa a lista\n");
+        printf("2 - Incluir no inicio\n");
+        printf("3 - Incluir no fim\n");
+        printf("4 - Incluir na posição\n");
+        printf("5 - Excluir do inicio\n");
+        printf("6 - Excluir do fim\n");
+        printf("7 - Excluir na posição\n\n");
+        printf("Opção: [ ]%c%c", 8, 8);
+        opcao = getche();
+        switch (opcao) {
+            case '0':
+                break;
+            case '1': limpaLista();
+                break;
+            case '2': insereInicio();
+                break;
+            case '3': insereFim();
+                break;
+            case '4': inserePosicao();
+                break;
+            case '5': retiraIncio();
+                break;
+            case '6': retiraFim();
+                break;
+            case '7': retiraPosicao();
+                break;
+            default:
+                mensagem("Opção inválido !!!");
+        }
+    } while (opcao != 0);
+    mensagem("Saindo do programa !!!");
+}
+
+elemento *criarNovo() {
+    elemento *novo = (elemento *) malloc(sizeof (elemento));
+    if (!novo) {
+        mensagem("Sem memória disponível !!!");
+        exit(1);
+    } else {
+        printf("\n\nNovo elemento: ");
+        novo->conteudo = getche();
+        novo->proximo = NULL;
+        return novo;
+    }
+}
+
+void insereFim() {
+    elemento *novo = criarNovo();
+    if (inicio == NULL) {
+        inicio = novo;
+    } else {
+        elemento *temp = inicio;
+        while (temp->proximo != NULL) {
+            temp = temp->proximo;
+        }
+        temp->proximo = novo;
+    }
+    numeroElementos++;
+}
+
+void insereInicio() {
+    elemento *novo = criarNovo();
+    elemento *inicioAnterior = inicio;
+    inicio = novo;
+    novo->proximo = inicioAnterior;
+    numeroElementos++;
+}
+
+void inserePosicao() {
+    int posicao;
+    printf("\n\nEm qual posição deseja inserir? ");
+    scanf("%d", &posicao);
+    if (posicao >= 1 && posicao <= numeroElementos) {
+        if (posicao == 1) {
+            insereInicio();
+        } else {
+            elemento *temp = inicio, *tempAnterior;
+            elemento *novo = criarNovo();
+            for (int i = 1; i < posicao; i++) {
+                tempAnterior = temp;
+                temp = temp->proximo;
+            }
+            tempAnterior->proximo = novo;
+            novo->proximo = temp;
+            numeroElementos++;
+        }
+    }
+}
+
+void limpaLista() {
+
+}
+
+void mensagem(const char* mensagem) {
+
+}
+
+void mostrarLista() {
+
+}
+
+void retiraIncio() {
+    if (inicio == NULL) {
+        mensagem("Não existe informação a ser retirada !!!");
+        return;
+    }
+    elemento *temp = inicio;
+    inicio = inicio->proximo;
+    free(temp);
+    numeroElementos--;
+}
+
+void retiraFim() {
+    if (inicio == NULL) {
+        mensagem("Não existe informação a ser retirada !!!");
+        return;
+    }
+    elemento *temp = inicio, *tempAnterior = inicio;
+    while (temp->proximo != NULL) {
+        tempAnterior = temp;
+        temp = temp->proximo;
+    }
+    if (inicio == temp){
+        inicio = NULL;
+    } else {
+        tempAnterior->proximo = NULL;
+    }
+    free(temp);
+    numeroElementos--;
+}
+
+void retiraPosicao() {
+    if (inicio == NULL) {
+        mensagem("Não existe informação a ser retirada !!!");
+        return;
+    }
+    int posicao;
+    printf("\n\nDe qual posição deseja retirar? ");
+    scanf("%d", &posicao);
+    if (posicao >1 && posicao >= numeroElementos) {
+        if (posicao == 1){
+            return retiraIncio();
+        } else {
+            elemento *temp = inicio, *tempAnterior;
+            for (int i = 1; i < posicao; ++i) {
+                tempAnterior = temp;
+                temp = temp->proximo;
+            }
+            tempAnterior->proximo = temp->proximo;
+            free(temp);
+            numeroElementos--;
+        }
+    } else {
+        mensagem("Posição invalida !!!");
+    }
+}
--- Estrutura de Dados/exercicioVetorString.cpp
+++ Estrutura de Dados/exercicioVetorString.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <string.h>
 
+main() {
+    char nome[7];
+    int s = 0;
+    strcpy(nome, "UNIPAR");
+    for (int i = 0; i < 7; i++){
+        printf("nome[%d] inicia em %X e tem o valor %d --> %c\n", i, &nome[i], nome[i], nome[i]);
+    }
+
+}
