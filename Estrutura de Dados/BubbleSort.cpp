3bd762e468e3b7b32089bf33b7932c5357cad2e3
marcos <marcosrossoni9@gmail.com>
2023-05-25 22:53:51 -0300

    add exemplo BubbleSort

---
+++ Estrutura de Dados/BubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <locale.h>
+
+const int tamanho = 6;
+char vetor[tamanho] = {'U', 'N',  'I', 'P', 'A', 'R'};
+char temp;
+int trocou;
+
+imprimeVetor() {
+    for (int i = 0; i < tamanho; ++i) {
+        printf("%c ", vetor[i]);
+    }
+}
+
+main () {
+    setlocale(LC_ALL, "Portuguese");
+    printf("Vetor antes da ordenação: ");
+    imprimeVetor();
+
+    do {
+        trocou = 0;
+        for (int i = 0; i < tamanho; ++i) {
+            if (vetor[i] > vetor[i + 1]) {
+                temp = vetor[i];
+                vetor[i] = vetor[i + 1];
+                vetor[i + 1] = temp;
+                trocou = 1;
+            }
+        }
+    } while (trocou == 1);
+
+    printf("\n\nVetor depois da ordenação: ");
+    imprimeVetor();
+}
